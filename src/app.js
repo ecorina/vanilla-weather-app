f9d124f2917ac122769bb1c3ad30854d6d15574f
ecorina <serban.corrina@gmail.com>
2021-11-10 18:22:11 +0000

    added css

--- src/app.js
+++ src/app.js
@@ -66,10 +66,9 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "6905ff0b38fa30ab82c89c7372272fbd";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+
   axios.get(apiUrl).then(displayForecast);
 }
 
